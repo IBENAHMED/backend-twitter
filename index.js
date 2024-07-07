a9de10cff780b06695e2d3481762b061a8b0f16b
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-07-07 23:13:09 +0100

    change domain

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+
 // Middleware
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // API Routes
 app.use('/', authRoutes);
