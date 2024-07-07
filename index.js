5830d68799936c5f1c46f14898aa61c6e27e1b9b
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-07-07 23:42:42 +0100

    change cors 2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+dotenv.config();
 import express from "express";
 import dotenv from "dotenv";
 import { authRoutes, userRoutes, postRoutes, notificationRoutes } from './routers/index.js';
 import { connectMongodb } from "./db/conectMongodb.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+const cors = require("cors");
 import { v2 as cloudinary } from 'cloudinary';
 
 
-dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,17 +19,13 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 4000;
 // var corsOptions = ;
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 // Middleware
 app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
 
 // API Routes
 app.use('/', authRoutes);
